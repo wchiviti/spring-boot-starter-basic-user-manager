e49f82efea326c4c8b43c1e3b4f8e7ffc3c65076
wchiviti <wilsonchiviti@gmail.com>
2020-01-21 14:15:01 +0200

    Adding auto configuration

--- src/main/java/io/github/hobbstech/commons/usermanager/BasicUserManagementConfiguration.java
+++ src/main/java/io/github/hobbstech/commons/usermanager/BasicUserManagementConfiguration.java
@@ -1,10 +1,8 @@
 package io.github.hobbstech.commons.usermanager;
 
-import io.github.hobbstech.commons.utilities.util.SystemProperties;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
